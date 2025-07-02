5a8f20853fb4ad9c3274ec5b4b86662b25309bcc
Lucasfi <lukafigueroa64@gmail.com>
2025-07-02 00:00:02 -0300

    creamos el componente Recetas y maquetamos card donde mostraremos las recetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Titulo from './components/Titulo'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Titulo from "./components/Titulo";
+import Recetas from "./components/Recetas";
 
 function App() {
-
   return (
     <>
-      <main className='container my-5'>
+      <main className="container my-5">
         <Titulo />
+        <div className="container my-3 row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 mx-2">
+          <Recetas />
+        </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
