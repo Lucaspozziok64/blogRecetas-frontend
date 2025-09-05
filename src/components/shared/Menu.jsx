1b0b6127a043dbc7da30e41cade2664575170670
Lucasfi <lukafigueroa64@gmail.com>
2025-09-05 20:51:30 -0300

    Agregue logo y un parrafo en el menu

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -2,17 +2,32 @@ import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { NavLink, Link, useNavigate } from "react-router";
 
 const Menu = ({ usuarioAdmin, setUsuarioAdmin }) => {
-  const navegacion = useNavigate()
+  const navegacion = useNavigate();
 
   const logout = () => {
-    setUsuarioAdmin(false)
-    sessionStorage.removeItem('userKey')
-    navegacion('/')
-  }
-  
+    setUsuarioAdmin(false);
+    sessionStorage.removeItem("userKey");
+    navegacion("/");
+  };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
+        <Navbar.Brand
+          as={Link}
+          to={"/"}
+          className="d-flex justify-content-center align-items-center"
+        >
+          <img
+            src={
+              "https://thumbs.dreamstime.com/b/dise%C3%B1o-gr%C3%A1fico-vectorial-del-libro-de-recetas-simple-creatividad-culinaria-explorar-nuestro-sencillo-logo-una-mezcla-simplicidad-321128647.jpg"
+            }
+            alt="logo Rolling Coffee"
+            className="img-fluid"
+            width={40}
+          />
+          <p className="mx-4 m-0 d-flex justify-content-center align-items-center">Blog de Recetas</p>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
@@ -24,7 +39,9 @@ const Menu = ({ usuarioAdmin, setUsuarioAdmin }) => {
                 <NavLink className="nav-link" to={"/administrador"}>
                   Administrador
                 </NavLink>
-                <Button className="nav-link" onClick={logout}>Logout</Button>
+                <Button className="nav-link" onClick={logout}>
+                  Logout
+                </Button>
               </>
             ) : (
               <NavLink className="nav-link" to={"/login"}>
